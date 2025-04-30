7dd84a6218e9338732448c5ae3be9c82504628de
EvanGunawan13 <evangunawan773@gmail.com>
2025-04-30 15:15:47 +0700

    laravel

---
+++ si4alaravel/app/Http/Controllers/FakultasController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Fakultas;
+use Illuminate\Http\Request;
+
+class FakultasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // panggil model fakultas menggunakan eloquent
+        $fakultas = Fakultas::all(); // perintah SQL select * from fakultas
+        //dd($fakultas); //dump and die
+        return view('fakultas.index', compact('fakultas'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Fakultas  $fakultas
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Fakultas $fakultas)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Fakultas  $fakultas
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Fakultas $fakultas)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Fakultas  $fakultas
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Fakultas $fakultas)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Fakultas  $fakultas
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Fakultas $fakultas)
+    {
+        //
+    }
+}
---
+++ si4alaravel/resources/views/fakultas/index.blade.php
@@ -0,0 +1,6 @@
+<h1>Fakultas</h1>
+
+@foreach ($collection as $item)
+{{ $item->nama }} {{ $item->singkatan }} <br>
+
+@endforeach
