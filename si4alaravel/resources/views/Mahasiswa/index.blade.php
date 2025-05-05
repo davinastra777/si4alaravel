54178c40f39018374cfd963ee8ca0b4fa50c3dce
EvanGunawan13 <evangunawan773@gmail.com>
2025-05-05 16:21:43 +0700

    p

--- si4alaravel/resources/views/Mahasiswa/index.blade.php
+++ si4alaravel/resources/views/Mahasiswa/index.blade.php
@@ -2,9 +2,9 @@
 @section('content')
 <h1>Mahasiswa</h1>
 @foreach ($mahasiswa as $item)
-{{$item->npm}} | {{
-    $item->nama}} | {{
-    $item->fakultas->nama}}
+    {{ $item->npm }} | 
+    {{ $item->nama }} | 
+    {{ $item->fakultas ? $item->fakultas->nama : 'Fakultas tidak tersedia' }} |
     <br>
-    @endforeach
-    @endsection
+@endforeach
+@endsection
